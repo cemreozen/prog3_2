485791c3901a4e9b48d04b3a9aba736832fbfe41
Cemre Oezen <cemreozen99@icloud.com>
2025-04-19 15:14:24 +0200

    fix

--- src/VariablennameAutomatTest.java
+++
@@ -1,9 +0,0 @@
-public class VariablennameAutomatTest {
-	public static void main(String[] args) {
-		VariablennameAutomat automat = new VariablennameAutomat();
-		System.out.println(automat.testen("ü"));
-		System.out.println(automat.testen("done_deal"));
-		System.out.println(automat.testen("_____"));
-		System.out.println(automat.testen("_code"));
-	}
-}
